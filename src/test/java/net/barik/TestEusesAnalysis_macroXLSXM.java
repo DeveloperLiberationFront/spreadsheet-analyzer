d97cf7b7de851af71348c03e519a4506c116d2cc
Justin Smith <jssmit11@ncsu.edu>
2015-02-09 11:14:45 -0500

    Add xlsm test

---
+++ src/test/java/net/barik/TestEusesAnalysis_macroXLSXM.java
@@ -0,0 +1,32 @@
+package net.barik;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestEusesAnalysis_macroXLSXM {
+	
+	private static SpreadsheetAnalyzer analyzer;
+
+	@BeforeClass
+	public static void loadSmallWorksheet() throws Exception{
+		InputStream is = TestEusesAnalysis_macroXLSXM.class.getResourceAsStream("/wmacro.xlsm");
+		assertNotNull(is);
+		InputStream is2 = TestEusesAnalysis_macroXLSXM.class.getResourceAsStream("/wmacro.xlsm");
+		assertNotNull(is2);
+		analyzer = SpreadsheetAnalyzer.doEUSESAnalysis(is, is2);
+		assertNotNull(analyzer);
+		
+	}
+
+    @Test
+    public void testContainsMacro() throws Exception{
+    	boolean b = analyzer.getContainsMacro();
+    	assertTrue(b);
+    }
+	
+}
